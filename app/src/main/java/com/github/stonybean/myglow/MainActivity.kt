8f14bfa0a84b845ae80614ca36128018abd202d4
stonybean <stonybean90@gmail.com>
2021-09-10 09:39:09 +0900

    initial commit

---
+++ app/src/main/java/com/github/stonybean/myglow/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.github.stonybean.myglow
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.github.stonybean.myglow.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_main, R.id.navigation_detail)
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+}
